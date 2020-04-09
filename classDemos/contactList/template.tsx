d8698f73e1c2690b93bc1a2d1f8b919da01b22ce
Fadi Atamny <fadiatamny@gmail.com>
2020-04-09 20:25:45 +0300

    template for components

---
+++ classDemos/contactList/template.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+
+const gridItem = (): any => {
+    return (
+        <View>
+            <Text> test</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default gridItem;
