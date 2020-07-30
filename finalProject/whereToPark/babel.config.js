0d58cbf35d0dc7d5bdbbfa365672043f13784204
Fadi Atamny <fadiatamny@gmail.com>
2020-07-30 21:37:42 +0300

    removed silly babel config that caused bugs when building

--- finalProject/whereToPark/babel.config.js
+++ finalProject/whereToPark/babel.config.js
@@ -2,7 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      '@babel/preset-env',
       '@babel/preset-react',
       'babel-preset-expo',
       '@babel/preset-typescript'
